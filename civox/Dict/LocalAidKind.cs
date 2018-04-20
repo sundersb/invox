2291162989c9dde0f9d22cfc2eedfd81b1764dcd
Alexander Belkov <m_sun@mail.ru>
2018-04-20 15:41:12 +1000

    Added medical aid kind dictionary

---
+++ civox/Dict/LocalAidKind.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace civox.Dict {
+    class LocalAidKind {
+        const string XML_NAME = "\\Dict\\V002-SLMSP.xml";
+
+        static LocalAidKind FInstance = null;
+        static object flock = new object();
+
+        static LocalAidKind Instance {
+            get {
+                if (FInstance == null) {
+                    lock (flock) {
+                        if (FInstance == null) {
+                            FInstance = new LocalAidKind();
+                            Helper.Load(FInstance.dict, Options.BaseDirectory + XML_NAME);
+                        }
+                    }
+                }
+                return FInstance;
+            }
+        }
+
+        Dictionary<string, string> dict;
+
+        LocalAidKind() {
+            dict = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Get federal code of the SMO
+        /// </summary>
+        /// <param name="value">Local SMO code</param>
+        /// <returns>Federal SMO code</returns>
+        public static string FromLocal(string value) {
+            return Instance.dict[value];
+        }
+    }
+}
