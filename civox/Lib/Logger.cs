430bc9e8df1b4915ba914ed42d9301fd7aba8abc
Alexander Belkov <m_sun@mail.ru>
2018-04-26 09:44:56 +1000

    Added log to file

--- civox/Lib/Logger.cs
+++ civox/Lib/Logger.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace civox.Lib {
     class Logger {
+        const string LOG_FILENAME = "\\civox.log";
+
+        static StreamWriter instance = null;
+        static object flock = new object();
+
+        static StreamWriter Instance {
+            get {
+                if (instance == null) lock (flock) {
+                    if (instance == null) {
+                        instance = new StreamWriter(Options.BaseDirectory + LOG_FILENAME, true, Encoding.UTF8);
+                    }
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Log message to console and to the logfile
+        /// </summary>
+        /// <param name="message">Message to log</param>
         public static void Log(string message) {
             Console.WriteLine(message);
+            Instance.WriteLine(message);
         }
     }
 }
