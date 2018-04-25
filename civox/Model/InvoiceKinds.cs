2415dec97898e58ecd7643192de59778d1aae10e
Alexander Belkov <m_sun@mail.ru>
2018-04-26 09:35:32 +1000

    Invoice kinds commented

--- civox/Model/InvoiceKinds.cs
+++ civox/Model/InvoiceKinds.cs
@@ -1,22 +1,31 @@
 ﻿namespace civox.Model {
+    /// <summary>
+    /// Код котрагента для формирования имени файла счета
+    /// </summary>
     enum AgentCode {
-        TerritoryFund,
-        AssuranceCompany,
-        Clinic
+        TerritoryFund,      // ХКФОМС
+        AssuranceCompany,   // СМО
+        Clinic              // ЛПУ
     }
 
+    /// <summary>
+    /// Вид счета для формирования имени файла счета
+    /// </summary>
     enum InvoiceKind {
-        GeneralTreatment,
-        HiTechAid,
-        Prophylaxis
+        GeneralTreatment,  // Посещения с лечебной целью
+        HiTechAid,         // ВМП
+        Prophylaxis        // Профилактические, диспансеризация
     }
 
+    /// <summary>
+    /// Вид счета на случаи с профцелью
+    /// </summary>
     enum ProfKind {
-        Stage1,
-        Stage2,
-        Adults,
-        Orphans,
-        Adopted,
-        Underage
+        Stage1,            // Диспансеризация 1 этап
+        Stage2,            // Диспансеризация 2 этап
+        Adults,            // Профосмотры взрослых
+        Orphans,           // Профосмотры детей-сирот
+        Adopted,           // Профосмотры на усыновления
+        Underage           // Профосмотры несовершеннолетних
     }
 }
