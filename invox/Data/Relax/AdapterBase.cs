f190cc44c0813b4acf72155f775c5073554709be
Alexander Belkov <m_sun@mail.ru>
2018-09-11 18:08:09 +1000

    Working over relax datapool

---
+++ invox/Data/Relax/AdapterBase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace invox.Data.Relax {
+    /// <summary>
+    /// Helper to extract a Model from SQL data reader or Models' collection from dataset
+    /// </summary>
+    /// <typeparam name="T">Model subclass</typeparam>
+    abstract class AdapterBase<T> {
+        /// <summary>
+        /// Read a Model from current dataset record
+        /// </summary>
+        /// <param name="reader">Data reader with Models' dataset</param>
+        /// <param name="number">Record's number in the dataset</param>
+        /// <returns>Model instance</returns>
+        public abstract T Read(DbDataReader reader, int number);
+
+        /// <summary>
+        /// Get collection of Models provided by dataset from an SQL command
+        /// </summary>
+        /// <param name="command">SQL command with select statement</param>
+        /// <returns>Models' collection</returns>
+        public IEnumerable<T> Load(DbCommand command) {
+            if (command.Connection.State == System.Data.ConnectionState.Closed)
+                command.Connection.Open();
+
+            DbDataReader r = null;
+            try {
+                r = command.ExecuteReader();
+            } catch (Exception ex) {
+                Lib.Logger.Log(ex.Message + "\r\n" + Pool.DescribeCommand(command));
+                if (r != null) r.Close();
+                command.Connection.Close();
+                yield break;
+            }
+
+            int i = 0;
+            while (r.Read()) {
+                ++i;
+                T record = Read(r, i);
+                if (record != null)
+                    yield return record;
+            }
+            r.Dispose();
+
+            command.Connection.Close();
+        }
+
+        /// <summary>
+        /// Get collection of Models by an SQL command composed on the fly
+        /// </summary>
+        /// <param name="connection">SQL connection to use</param>
+        /// <param name="sql">Text of the SQL query</param>
+        public IEnumerable<T> Load(DbConnection connection, string sql) {
+            DbCommand command = connection.CreateCommand();
+            command.CommandText = sql;
+
+            command.Connection.Open();
+
+            DbDataReader r = null;
+            try {
+                r = command.ExecuteReader();
+            } catch (Exception ex) {
+                Lib.Logger.Log(ex.Message + "\r\n" + Pool.DescribeCommand(command));
+                if (r != null) r.Close();
+            }
+
+            try {
+                int i = 0;
+                while (r.Read()) {
+                    ++i;
+                    T record = Read(r, i);
+                    if (record != null)
+                        yield return record;
+                }
+            } finally {
+                r.Dispose();
+            }
+        }
+
+        protected int ReadInt(object value) {
+            int result = 0;
+            if (value != DBNull.Value) int.TryParse((string)value, out result);
+            return result;
+        }
+
+        protected string ReadString(object value) {
+            if (value == DBNull.Value) return string.Empty;
+
+            string result = (string)value;
+            return result.Trim();
+        }
+
+        protected DateTime ReadDate(object value) {
+            if (value != DBNull.Value)
+                return (DateTime)value;
+            else
+                return new DateTime(1900, 1, 1);
+        }
+    }
+}
---
+++ invox/Data/Relax/Pool.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Data.Common;
+
+namespace invox.Data.Relax {
+    class Pool : IInvoice {
+        const string CONNECTION_STRING = "Provider=vfpoledb;Data Source={0};Collating Sequence=machine;Mode=ReadWrite|Share Deny None;";
+        const string PERIOD_MARKER = "{period}";
+        const string LPU_MARKER = "{lpu}";
+
+        string period;
+        OleDbConnection connectionMain;
+
+        AdapterStrings aStrings;
+
+        public Pool(string location, string period) {
+            this.period = period;
+            string cs = string.Format(CONNECTION_STRING, location);
+            connectionMain = new OleDbConnection(cs);
+        }
+
+        /// <summary>
+        /// Add varchar parameters to an SQL command
+        /// </summary>
+        /// <param name="command">SQL command to add parameters to</param>
+        /// <param name="parameters">Parameter names</param>
+        void AddStringParameters(OleDbCommand command, string[] parameters) {
+            foreach (string param in parameters)
+                command.Parameters.Add(new OleDbParameter(param, OleDbType.VarChar));
+        }
+
+        /// <summary>
+        /// Get SQL command query and parameter values
+        /// </summary>
+        /// <param name="command">Command to describe</param>
+        /// <returns>Command's SQL text and parameters</returns>
+        public static string DescribeCommand(DbCommand command) {
+            StringBuilder sb = new StringBuilder(command.CommandText);
+            if (command.Parameters.Count > 0) {
+                sb.Append("\r\n\r\nПараметры:");
+                foreach (DbParameter p in command.Parameters) {
+                    sb.Append(string.Format("\r\n\t{0} = '{1}'", p.ParameterName, p.Value));
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Execute a non-select query
+        /// </summary>
+        static void ExecuteCommand(DbCommand command) {
+            if (command.Connection.State != System.Data.ConnectionState.Open)
+                command.Connection.Open();
+            try {
+                command.ExecuteNonQuery();
+            } catch (Exception ex) {
+                Lib.Logger.Log(ex.Message + "\r\n" + DescribeCommand(command));
+            } finally {
+                command.Connection.Close();
+            }
+        }
+        
+        /// <summary>
+        /// Execute SQL select command to get a single value
+        /// </summary>
+        static object ExecuteScalar(DbCommand command) {
+            if (command.Connection.State != System.Data.ConnectionState.Open)
+                command.Connection.Open();
+            object result = null;
+
+            try {
+                result = command.ExecuteScalar();
+            } catch (Exception ex) {
+                Lib.Logger.Log(ex.Message + "\r\n" + DescribeCommand(command));
+            } finally {
+                command.Connection.Close();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Execute arbitrary select query
+        /// </summary>
+        /// <param name="command">Command to execute</param>
+        /// <param name="onRead">Action to perform on records in the dataset</param>
+        static void ExecuteReader(DbCommand command, Action<DbDataReader> onRead) {
+            command.Connection.Open();
+            try {
+                DbDataReader r = null;
+                try {
+                    r = command.ExecuteReader();
+                } catch (Exception ex) {
+                    Lib.Logger.Log(ex.Message + "\r\n" + DescribeCommand(command));
+                    if (r != null) r.Close();
+                }
+
+                try {
+                    while (r.Read())
+                        onRead(r);
+                } finally {
+                    r.Dispose();
+                }
+            } finally {
+                command.Connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Execute arbitrary select query
+        /// </summary>
+        /// <param name="connection">Connection to use</param>
+        /// <param name="sql">Command to execute</param>
+        /// <param name="onRead">Action to perform on records in the dataset</param>
+        static void ExecuteReader(DbConnection connection, string sql, Action<DbDataReader> onRead) {
+            DbCommand command = connection.CreateCommand();
+            command.CommandText = sql;
+            command.Connection.Open();
+            DbDataReader r = null;
+            try {
+                r = command.ExecuteReader();
+            } catch (Exception ex) {
+                Lib.Logger.Log(ex.Message + "\r\n" + DescribeCommand(command));
+                if (r != null) r.Close();
+            }
+
+            try {
+                while (r.Read())
+                    onRead(r);
+            } finally {
+                r.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Extend SQL macros
+        /// </summary>
+        /// <param name="sql">SQL with period and clinic macros</param>
+        /// <returns>SQL with macros extended</returns>
+        string LocalizeQuery(string sql) {
+            return sql.Replace(PERIOD_MARKER, Options.PeriodLocation)
+                .Replace(LPU_MARKER, Options.LocalLpuCode);
+        }
+
+        public IEnumerable<Model.OnkologyDiagnosticType> LoadOnkologicalDiagnosticTypes() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.OnkologyRefusal> LoadOnkologicalRefusal() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.ComplexityQuotient> LoadComplexityQuotients() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> LoadConcurrentDiagnoses() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> LoadComplicationDiagnoses() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> LoadMesCodes() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> LoadPersonDiagnoses() {
+            throw new NotImplementedException();
+        }
+
+        public Model.OnkologyTreat GetOnkologyTreat() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Sanction> LoadSanctions() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Service> LoadServices() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.OncologyDirection> LoadOncologyDirections() {
+            throw new NotImplementedException();
+        }
+
+        public Model.OncologyService GetOncologyService() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Event> LoadEvents() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.ConcomitantDisease> GetConcomitantDiseases() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.DispAssignment> GetDispanserisationAssignmetns() {
+            throw new NotImplementedException();
+        }
+
+        public int GetPeopleCount(Model.OrderSection section) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Person> LoadPeople(Model.OrderSection section) {
+            throw new NotImplementedException();
+        }
+
+        public int GetInvoiceRecordsCount(Model.OrderSection section) {
+            throw new NotImplementedException();
+        }
+
+        public float Total(Model.OrderSection section) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.InvoiceRecord> LoadInvoiceRecords(Model.OrderSection section) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> LoadNoDeptDoctors() {
+            return aStrings.Load(connectionMain, "");
+        }
+    }
+}
